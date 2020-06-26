5b4e34e51e06ecd9b060c70c5f167901c551d957
DenisNest <66721699+DenisNest@users.noreply.github.com>
2020-06-26 19:12:57 +0300

    add task1 les_foyr

--- les_four.py
+++ les_four.py
@@ -1,4 +1,16 @@
 print('task 1')
-art = 0
-lil = 2
+def my_calc():
+    hh = float(input('Введите количество оработанных часов: '))
+    sum_h = float(input('Введите сумму оплаты труда в час: '))
+    prize = float(input('Введите размер премии: '))
+    result = hh * sum_h
+    return result + prize
+print(f' Размер заработной платы составил: {my_calc() }')
+
+print('task 2')
+
+
+
+
+
 
